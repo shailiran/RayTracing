0eff27bf1a2578d8a519ece5bd182c51dbbbdbb4
shailiran <shailiran@gmail.com>
2021-04-12 16:37:36 +0300

    findIntersection

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,19 +1,35 @@
 package src;
+import java.util.List;
 
 public class Intersection {
     double minT;
     Surfaces minSurface;
+    boolean hasIntersection;
 
-    public Intersection(double minT, Surfaces minSurface) {
+    public Intersection(double minT, Surfaces minSurface, boolean hasIntersection) {
         this.minT = minT;
         this.minSurface = minSurface;
+        this.hasIntersection = hasIntersection;
     }
 
+    public boolean getHasIntersection() {
+        return this.hasIntersection;
+    }
     public static Intersection findIntersection(Ray ray, Scene scene) {
-        Intersection intersection;
-        double t, minT;
-
-
-        return intersection;
+        double t, minT = Double.MAX_VALUE;
+        List<Surfaces> surfaces = scene.getSurfaces();
+        Surfaces closestSurface = null;
+        boolean hasIntersection = false;
+        for (Surfaces surface: surfaces) {
+            t = surface.intersection(ray);
+            if (t > 0 && t < minT) {
+                minT = t;
+                closestSurface = surface;
+                hasIntersection = true;
+            }
+        }
+        return new Intersection(minT, closestSurface, hasIntersection);
     }
+
+
 }
